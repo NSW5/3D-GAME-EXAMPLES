d7459abc2267c820c901df26e03647685d8888cf
NSW5 <146126932+NSW5@users.noreply.github.com>
2024-02-14 12:07:30 -0800

    Finished ground and walls

--- 3D GAME EXAMPLES/Assets/Scripts/MovingWJohn.cs
+++ 3D GAME EXAMPLES/Assets/Scripts/MovingWJohn.cs
@@ -4,31 +4,60 @@ using UnityEngine;
 
 public class MovingWJohn : MonoBehaviour
 {
+    public int score = 0;
     public float turnSpeed = 20;
-    public float moveSpeed = 1;
+    public float moveSpeed = 1f;
     public float JumpForce = 10f;
-    public float outOfBounds = -10f;
-    public GameObject checkPointAreaObject;
-    private Vector3 _Movement;
     public float GravityModifier = 1f;
+    public float outOfBounds = -10f;
     public bool IsOnGround = true;
     public bool isAtCheckpoint = false;
-
-    //private Animator m_Animator;
-    private Rigidbody _Rigidbody;
-    private Quaternion _Rotation = Quaternion.identity;
+    public GameObject checkpointAreaObject;
+    public GameObject finishAreaObject;
+    private Vector3 _movement;
+    //private Animator _animator;
+    private Rigidbody _rigidbody;
+    private Quaternion _rotation = Quaternion.identity;
     private Vector3 _defaultGravity = new Vector3(0f, -9.81f, 0f);
-    private Vector3 _startingPosition;
+    private Vector3 _startingPostion;
     private Vector3 _checkpointPosition;
+    private GameObject[] _collectibles;
 
     // Start is called before the first frame update
     void Start()
     {
-        //_Animator = GetComponent<Animator>();
-        _Rigidbody = GetComponent<Rigidbody>();
+        //_animator = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody>();
         Physics.gravity = _defaultGravity;
+        //Debug.Log(Physics.gravity);
         Physics.gravity *= GravityModifier;
-        _startingPosition = transform.position;
+        //Debug.Log(Physics.gravity);
+        _startingPostion = transform.position;
+        _collectibles = GameObject.FindGameObjectsWithTag("Collectible-Return");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && IsOnGround)
+        {
+            _rigidbody.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+            IsOnGround = false;
+        }
+
+        if (transform.position.y < outOfBounds)
+        {
+            if (isAtCheckpoint)
+            {
+                ReturningCollectibles();
+                transform.position = _checkpointPosition;
+            }
+            else
+            {
+                ReturningCollectibles();
+                transform.position = _startingPostion;
+            }
+
+        }
     }
 
     // Update is called once per frame
@@ -37,25 +66,26 @@ public class MovingWJohn : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        _Movement.Set(horizontal, 0f, vertical);
-        _Movement.Normalize();
+        _movement.Set(horizontal, 0f, vertical);
+        _movement.Normalize();
 
         bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
         bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
         bool isWalking = hasHorizontalInput || hasVerticalInput;
-        //m_Animator.SetBool("IsWalking", isWalking);
+        //_animator.SetBool ("IsWalking", isWalking);
 
-        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, _Movement, turnSpeed * Time.deltaTime, 0f);
-        _Rotation = Quaternion.LookRotation(desiredForward);
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, _movement, turnSpeed * Time.deltaTime, 0f);
+        _rotation = Quaternion.LookRotation(desiredForward);
 
-        _Rigidbody.MovePosition(_Rigidbody.position + _Movement * moveSpeed * Time.deltaTime);
-        _Rigidbody.MoveRotation(_Rotation);
+        _rigidbody.MovePosition(_rigidbody.position + _movement * moveSpeed * Time.deltaTime);
+        _rigidbody.MoveRotation(_rotation);
+
     }
 
-    //void OnAnimatorMove()
+    //void OnAnimatorMove ()
     //{
-    //m_Rigidbody.MovePosition(_Rigidbody.position + _Movement * //m_Animator.deltaPosition.magnitude);
-    //m_Rigidbody.MoveRotation(m_Rotation);
+    //    _rigidbody.MovePosition (_rigidbody.position + _movement * m_Animator.deltaPosition.magnitude);
+    //    _rigidbody.MoveRotation (_rotation);
     //}
 
     private void OnCollisionEnter(Collision collision)
@@ -64,37 +94,57 @@ public class MovingWJohn : MonoBehaviour
         {
             IsOnGround = true;
         }
-    }
 
-    void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject == checkPointAreaObject)
-        {
-            isAtCheckpoint = true;
-            //Debug.Log(_startingPosition);
-            _checkpointPosition = checkPointAreaObject.transform.position;
-            //Debug.Log(_startingPosition);
-        }
-    }
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space) && IsOnGround)
-        {
-            _Rigidbody.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
-            IsOnGround = false;
-        }
-
-        if(transform.position.y < outOfBounds)
+        if (collision.gameObject.CompareTag("Spinner"))
         {
             if (isAtCheckpoint)
             {
-                transform.position = _checkpointPosition;
+                ReturningCollectibles();
+                transform.position = checkpointAreaObject.transform.position;
+
             }
             else
             {
-                transform.position = _startingPosition;
+                ReturningCollectibles();
+                transform.position = _startingPostion;
             }
+        }
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == checkpointAreaObject)
+        {
+            isAtCheckpoint = true;
+            _checkpointPosition = checkpointAreaObject.transform.position;
+        }
+
+        if (other.gameObject == finishAreaObject)
+        {
+            isAtCheckpoint = false;
+            ReturningCollectibles();
+            transform.position = _startingPostion;
+        }
+
+        if (other.gameObject.CompareTag("Collectible-Destroy"))
+        {
+            score++;
+            Destroy(other.gameObject);
+        }
+
+        if (other.gameObject.CompareTag("Collectible-Return"))
+        {
+            score++;
+            other.gameObject.GetComponent<Collectiable>().HideCollectibles();
+        }
+    }
+
+    void ReturningCollectibles()
+    {
+        for (int i = 0; i < _collectibles.Length; i++)
+        {
+            _collectibles[i].SetActive(true);
+            _collectibles[i].GetComponent<Collectiable>().ReturnCollectibles();
         }
     }
 }
--- 3D GAME EXAMPLES/Assets/Scripts/Spinner.cs
+++ 3D GAME EXAMPLES/Assets/Scripts/Spinner.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Spinner : MonoBehaviour
 {
+    public float rotationSpeed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
     }
 }
